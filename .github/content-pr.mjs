becad2d2688dc4cb21c769a6c80ad879cc194b9e
Emma Hamilton <git@emmas.town>
2024-06-05 10:43:07 +1000

    Try a thing

---
+++ .github/content-pr.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ *
+ * @param {ReturnType<typeof import('@actions/github').getOctokit>} github
+ */
+export async function createOrUpdateContentPR(github) {
+  console.log(process.env);
+}
